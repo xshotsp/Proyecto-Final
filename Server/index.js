304f9187a3bb6311842e38ee5f360886e6cae404
lmlenis <llenis73@gmail.com>
2023-11-22 18:25:09 -0500

    correccion rutas

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 const { apiLoaderProducts } = require("./src/apiLoaderProducts.js");
 const PORT = 3001;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
 server.listen(PORT, async() => {
   await apiLoaderProducts()
   console.log(`Server listening on port ${PORT}`);
