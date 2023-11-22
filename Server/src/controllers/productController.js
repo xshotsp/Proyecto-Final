e4c977c3b0ddf318b3b09d6864608f152abae54c
lmlenis <llenis73@gmail.com>
2023-11-22 10:10:55 -0500

    creacion controllers handlers de producto

---
+++ Server/src/controllers/productController.js
@@ -0,0 +1,155 @@
+const { Product } = require("../db");
+//const cloudinary = require('cloudinary').v2;
+
+//*********************************************************** */
+// trae todos los productos de la base de datos
+const getAllProducts = async () => {
+  
+  const productsDB = await Product.findAll();
+  return productsDB;
+};
+//*********************************************************** */
+
+
+// trae el procucto del id correspondiente...se usa para el detalle
+const getProductsById = async (id) => {
+  const productDB = await Product.findByPk(id);
+  return productDB;
+};
+
+/**************************************************************** */
+// se usa para busqueda por nombre
+const getProductByName = async (name) => {
+  const productDB = await Product.findAll({
+    where: {
+      name: {
+        [Op.iLike]: `%${name}%`, 
+      },
+    },
+  });
+  if (productDB.length === 0) {
+    return [
+      { message: "No se encontraron prductos que coincidan con la búsqueda." },
+    ];
+  }
+
+  return productDB;
+};
+
+/************************************************************************* */
+// se usa para crear el producto
+const createProducts = async (productData) => {
+  console.log (productData)
+  try {
+    const {
+      name,
+      image,
+      price,
+      color,
+      additionalImage,
+    } = productData;
+
+    const productCreated = await Product.findOne ({where: {name:name, price: price, color:color}})
+    if(productCreated) {
+      throw new Error ('Un producto ya existe con esas caracteristicas')
+    }
+    
+     // CLOUDINARY
+    //   const cloudinaryUpload = await cloudinary.uploader.upload(`${image}`);
+    //   const img = cloudinaryUpload.secure_url;
+      
+
+    const newProduct = await Product.create({
+        name,
+        image,
+        price,
+        color,
+        additionalImage,
+    });
+    
+    return newProduct;
+  } catch (error) {
+    console.log(error)
+    throw error;
+  }
+};
+
+/**************************************************************************** */
+
+// para borrar un producto con un id especifico
+const deleteProductById = async (id) => {
+  try {
+    const productToDelete = await Product.findByPk(id);
+
+    if (!productToDelete) {
+      throw new Error(`Producto con ID ${id} no encontrado.`);
+    }
+
+    await productToDelete.destroy();
+
+    return `Producto con ID ${id} eliminado exitosamente.`;
+  } catch (error) {
+    throw error;
+  }
+};
+
+/**************************************************************************** */
+// restaurar del borrado lógico
+const restoreProductById = async (id) => {
+  try {
+    const restoredProduct = await Product.restore({ where: { id } });
+    return restoredProduct; 
+  } catch (error) {
+    throw error; 
+  }
+};
+
+/******************************************************************************* */
+// Para editar o actualizar un producto con un id especifico
+const updateProductById = async (id, newData) => {
+  
+  try {
+    const {
+        name,
+        image,
+        price,
+        color,
+        additionalImage,
+    } = newData;
+    const productToUpdate = await Product.findByPk(id);
+    
+    if (!productToUpdate) {
+      throw new Error(`Producto con ID ${id} no encontrado.`);
+    }
+    
+    // CLOUDINARY
+    // const cloudinaryUpload = await cloudinary.uploader.upload(`${image}`);
+    // const img = cloudinaryUpload.secure_url;
+    
+       
+    // Actualiza los campos del producto con los nuevos datos
+
+    await productToUpdate.update({
+      id,
+      name,
+      image,
+      price,
+      color,
+      additionalImage,
+    });
+
+    return productToUpdate;
+  } catch (error) {
+    throw error;
+  }
+};
+/*************************************************************************************** */
+module.exports = {
+  getAllProducts,
+  getProductsById,
+  getProductByName,
+  createProducts,
+  deleteProductById,
+  updateProductById,
+  restoreProductById
+};
