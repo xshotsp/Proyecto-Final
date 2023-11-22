eba9b015b31d625e8df6cc7c1b533e5c8488bd9b
lmlenis <llenis73@gmail.com>
2023-11-22 14:36:47 -0500

    adicion de rutas

--- Server/src/routes/router.js
+++ Server/src/routes/router.js
@@ -1,6 +1,8 @@
 // router.js
 const express = require('express');
 const router = express.Router();
+const {getAllProductsHandler, getIdHandler, getProductsByName,createProductsHandler,
+  deleteProductsHandler,updateProductsHandler, restoreProductHandler} = require("../handlers/productHandler")
 
 // Ruta de ejemplo
 router.get('/', (req, res) => {
@@ -8,5 +10,13 @@ router.get('/', (req, res) => {
 });
 
 // Otras rutas pueden agregarse aquí
+// Rutas para la tabla product *****************************/
+router.get("product/", getAllProductsHandler);
+router.get("product/:id", getIdHandler);
+router.get("product/name/:name",getProductsByName);
+router.post("product/",createProductsHandler);
+router.post('/product/restore/:id', restoreProductHandler); // esto es para el borrado logico
+router.delete("product/delete/:id", deleteProductsHandler);
+router.put("product/put/:id", updateProductsHandler);
 
 module.exports = router;
