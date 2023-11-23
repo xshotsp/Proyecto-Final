cc6abdce05ca2591389b7f7760557fd0a0f93f80
Juan Gallego <gallegoj135@gmail.com>
2023-11-23 17:48:13 -0500

    componente form

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store/store.js';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
