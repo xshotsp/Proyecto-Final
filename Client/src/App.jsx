9711ae9a1378d01af6f0b23ed7c51f4d364a6418
Juan Gallego <gallegoj135@gmail.com>
2023-11-23 17:30:18 -0500

    ruta form

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes, useLocation} from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import HomePage from './Components/Home/HomePage'
 import Footer from './Components/Footer/Footer'
-
+import FormPage from './components/formpage/FormPage'
 function App() {
   const pathname = useLocation()
   return (
@@ -12,6 +12,7 @@ function App() {
           {pathname === '/' ? null : <NavBar/>}
       <Routes>
             <Route path='/' element={<HomePage />}/>
+            <Route path='/form' element={<FormPage/>}></Route>
       </Routes>
       <Footer/>
     </div>
