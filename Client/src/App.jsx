210c53689d01280634dec22fd2f46064a0662e0b
eliaspucheta <eliaspucheta11@gmail.com>
2023-11-23 15:38:09 -0300

    footer prueba

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Route, Routes, useLocation} from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import HomePage from './Components/Home/HomePage'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   const pathname = useLocation()
@@ -12,6 +13,7 @@ function App() {
       <Routes>
             <Route path='/' element={<HomePage />}/>
       </Routes>
+      <Footer/>
     </div>
   )
 }
