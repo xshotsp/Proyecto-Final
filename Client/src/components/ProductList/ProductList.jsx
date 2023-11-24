14eb2debbd5661898307321f5e0bd86b4fc1ba51
lmlenis <llenis73@gmail.com>
2023-11-24 12:33:40 -0500

    buscar por nombre funcionando

--- Client/src/components/ProductList/ProductList.jsx
+++ Client/src/components/ProductList/ProductList.jsx
@@ -1,34 +1,55 @@
 /* eslint-disable react/prop-types */
 // ProductList.jsx
-import  { useEffect, useState } from 'react';
+import  { useEffect } from 'react';
+import { useSelector, useDispatch} from 'react-redux';
 import { getAllProducts } from '../../redux/actions/actions';
 import s from './ProductList.module.css';
 import Card from '../card/Card';
 
 const ProductList = () => {
-  const [products, setProducts] = useState([]);
+  //const [products, setProducts] = useState([]);
+  const products = useSelector ((state) => state.allproducts)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const productsData = await getAllProducts();
-        setProducts(productsData);
-      } catch (error) {
-        console.error('Error al obtener productos:', error);
-      }
-    };
+    // const fetchData = async () => {
+    //   try {
+    //     const productsData = await getAllProducts();
+    //     //setProducts(productsData);
+    //   } catch (error) {
+    //     console.error('Error al obtener productos:', error);
+    //   }
+    // };
 
-    fetchData();
-  }, []);
+    // fetchData();
+    dispatch(getAllProducts());
+  }, [dispatch]);
 
+  if (products[0]?.name) console.log ('hola')
+  else console.log('adios')
+  
+  console.log(products)
   return (
     <div>
-      <h1>Lista de Productos</h1>
+      {
+      (products[0]?.message) && <h2>{products[0].message}</h2>
+      }
+      
+      {
+       (products[0]?.name) && <h1>Lista de Productos</h1>
+      }
+      
+      {
+        (products[0]?.name) &&
+      
       <div className={s.productList}>
         {products.map((product) => (
           <Card key={product.id} product={product} />
         ))}
       </div>
+      }
+
+  
     </div>
   );
 };
