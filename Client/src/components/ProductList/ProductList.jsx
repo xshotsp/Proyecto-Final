bb03b1d87f7c1d5f9a3658f88afa753e25a291d2
eliaspucheta11 <eliaspucheta11@gmail.com>
2023-11-29 17:09:41 -0800

    data ignore

--- Client/src/components/ProductList/ProductList.jsx
+++ Client/src/components/ProductList/ProductList.jsx
@@ -10,25 +10,25 @@ import { getAllProducts, getProducts } from '../../redux/actions/actions';
 import s from './ProductList.module.css';
 import Card from '../card/Card';
 import Pagination from '../pagination/Pagination';
-//import data from '../data/data';
+import data from '../data/data';
 
 const ProductList = ({ handleAddProduct }) => {
   
   const dispatch = useDispatch()
-  const products = useSelector ((state) => state.allproducts)
+  //const products = useSelector ((state) => state.allproducts)
   
   const [currentPage, setCurrentPage] = useState(1);
   const cardsPerPage = 5;
   const lastCardIndex = currentPage * cardsPerPage;
   const firstCardIndex = lastCardIndex - cardsPerPage;
-  const currentCards = products.slice(firstCardIndex, lastCardIndex);
+  const currentCards = data.slice(firstCardIndex, lastCardIndex);
 
   // useEffect(() => {
   //    dispatch(getAllProducts());
   //    setCurrentPage(1)
   // }, [dispatch]);
   useEffect(() => {
-    if(products.length === 0) dispatch(getAllProducts());
+    if(data.length === 0) dispatch(getAllProducts());
     console.log(getAllProducts);
     setCurrentPage(1)
  }, []);
@@ -36,15 +36,15 @@ const ProductList = ({ handleAddProduct }) => {
   return (
     <div>
       {
-      (products[0]?.message) && <h2>{products[0].message}</h2>
+      (data[0]?.message) && <h2>{data[0].message}</h2>
       }
       
       {
-       (products[0]?.name) && <h1>Lista de Productos</h1>
+       (data[0]?.name) && <h1>Lista de Productos</h1>
       }
       
       {
-        (products[0]?.name) &&
+        (data[0]?.name) &&
       
       <div className={s.productList}>
         {/* {products.map((product) => ( */}
@@ -57,7 +57,7 @@ const ProductList = ({ handleAddProduct }) => {
   
 
       <Pagination
-      filteredCountries={products} //cambio
+      filteredCountries={data} //cambio
       cardsPerPage={cardsPerPage}
       setCurrentPage={setCurrentPage}
       currentPage={currentPage}/>
