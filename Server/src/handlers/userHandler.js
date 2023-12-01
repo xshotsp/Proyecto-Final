a3c5348d909c9c4f03bffd82c8b7f1ede76fe475
lmlenis <llenis73@gmail.com>
2023-11-30 19:51:06 -0500

    enviar mails cuando se registre usuario

--- Server/src/handlers/userHandler.js
+++ Server/src/handlers/userHandler.js
@@ -1,5 +1,10 @@
 const { getUser, getOneUser } = require("../controllers/userController");
 const { User } = require("../db");
+const transporter = require("../functions/sendMails");
+const cloudinary = require("cloudinary").v2;
+const bcrypt = require("bcrypt");
+
+
 const getUserHandler = async (req, res) => {
   try {
     const results = await getUser();
@@ -34,7 +39,7 @@ const putUserHandler = async (req, res) => {
 
 const createUserHandler = async (req, res) => {
   try {
-    const { username, password, email, profile_picture, member } = req.body;
+    let { username, password, email, profile_picture, member } = req.body;
 
     if (!username || !password || !email) {
       return res.status(400).json("Campos obligatorios incompletos.");
@@ -54,6 +59,17 @@ const createUserHandler = async (req, res) => {
     if (searchUser.length || searchEmail.length) {
        return res.status(404).json("El usuario o el correo electrónico ya existe.");
     } else {
+
+       // CLOUDINARY
+      if (profile_picture){
+        const cloudinaryUpload = await cloudinary.uploader.upload(`${profile_picture}`);
+        profile_picture = cloudinaryUpload.secure_url;
+      }
+
+      // para encriptar el password
+      const hashedPassword = await bcrypt.hash(password, 10);
+      password = hashedPassword;
+
       const newUser = await User.create({
         username,
         password,
@@ -61,6 +77,27 @@ const createUserHandler = async (req, res) => {
         profile_picture,
         member,
       });
+
+
+      await transporter.sendMail({
+        from: "mensaje enviado por <quirkz41@gmail.com>",
+        to: email,
+        subject: "Bienvenid@ a QUIRKZ",
+        html:` 
+        <h2>${username}</h2>
+        <p>Mensaje de Bienvenida de nuestra tienda online QUIRKZ</p>
+        <p style="font-size: 16px; color: #0074d9;">
+      Para ir a la pagina, haz clic <a href="http://localhost:5173" style="text-decoration: none; color: #ff4136; font-weight: bold;">aquí</a>.
+    </p>` ,
+      })
+    
+
+
+
+
+
+      
+
       return res.status(200).json(newUser);
     }
   } catch (error) {
