64a02f4b92dbcf6095f0f92326d7b77af450596c
Juan Gallego <gallegoj135@gmail.com>
2023-12-03 18:01:13 -0500

    version actualizada

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
@@ -8,62 +9,63 @@ import FormPage from "./components/formpage/FormPage";
 import Login from "./components/Login/Login";
 import DetailPage from "./components/detailpage/DetailPage";
 import Cart from "./components/Cart/Cart";
-import { useSelector, useDispatch } from 'react-redux';
-import {toggleDarkMode} from './redux/actions/actions'
 import { useEffect, useState } from "react";
 import Contact from "./components/Contact/Contact";
 import Error404 from "./components/Error/Error404";
-const handleAddProduct = (product) => {
-  const ProductExist = cartItems.find((item) => item.id === product.id);
-  if(ProductExist){
-    setCartItems(cartItems.map((item) => item.id === product.id 
-    ? {...ProductExist, quantity: ProductExist.quantity + 1}
-    : item
-      )
-    );
-  } else {
-    setCartItems([...cartItems, { ...product, quantity: 1 }]);
-  }
-};
 
-const handleRemoveProduct = (product) => {
-  const ProductExist = cartItems.find((item) => item.id === product.id);
-  if(ProductExist.quantity === 1){
-    setCartItems(cartItems.filter((item) => item.id !== product.id));
-  } else {
-    setCartItems(
-      cartItems.map((item) => item.id === product.id 
-      ? {...ProductExist, quantity: ProductExist.quantity - 1} 
-      : item
-      )
-    );
-  }
-};
-
-const handleClearCart = () => {
-  setCartItems([]);
-}
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-  const darkMode = useSelector(state => state.darkMode); 
-  const [isLoggedIn, setLoggedIn] = useState(false);
-  const dispatch = useDispatch();
+  
+  const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart") || '[]')
+  const [cartItems, setCartItems] = useState(cartFromLocalStorage);
 
   useEffect(() => {
-    const body = document.body;
-    body.classList.toggle('darkMode', darkMode);
-    body.classList.toggle('lightMode', !darkMode);
-  }, [darkMode]);
+    localStorage.setItem("cart", JSON.stringify(cartItems))
+  }, [cartItems])
+
+  const handleAddProduct = (product) => {
+    const ProductExist = cartItems.find((item) => item.id === product.id);
+    if(ProductExist){
+      setCartItems(cartItems.map((item) => item.id === product.id 
+      ? {...ProductExist, quantity: ProductExist.quantity + 1}
+      : item
+      )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const handleRemoveProduct = (product) => {
+    const ProductExist = cartItems.find((item) => item.id === product.id);
+    if(ProductExist.quantity === 1){
+      setCartItems(cartItems.filter((item) => item.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((item) => item.id === product.id 
+        ? {...ProductExist, quantity: ProductExist.quantity - 1} 
+        : item
+        )
+      );
+    }
+  };
+
+  const handleClearCart = () => {
+    setCartItems([]);
+  }
 
   return (
-    <div className='App'>
-      <NavBar darkMode={darkMode} setDarkMode={() => dispatch(toggleDarkMode())} isLoggedIn={isLoggedIn} setLoggedIn={setLoggedIn} />
+    <div className="App">
+      <NavBar cartItems={cartItems} />
       <Routes>
-        <Route path="/" element={<HomePage handleAddProduct={(product) => handleAddProduct(cartItems, setCartItems, product)} />} />
-        <Route path="/product/:id" element={<DetailPage />} />
+        <Route path="/" element={<HomePage
+        handleAddProduct={handleAddProduct} 
+        cartItems={cartItems} />} />
+        <Route path="/product/:id" element={<DetailPage
+        handleAddProduct={handleAddProduct} />} />
+        <Route path="/contacto" element={<Contact />} />
         <Route path="/form" element={<FormPage />} />
-        <Route path="/login" element={<Login setLoggedIn={setLoggedIn} isLoggedIn={isLoggedIn} />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/createuser" element={<CreateUserForm />} />
         <Route path="*" element={<Error404 />} />
         <Route path="/cart"
