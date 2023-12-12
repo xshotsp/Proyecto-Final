edf3eb73ee04814dcc0b28b9e57f848d070b1f92
lmlenis <llenis73@gmail.com>
2023-12-12 11:32:52 -0500

    finalizacion compra y mis compras

--- Client/src/components/myShooping/MyShooping.jsx
+++ Client/src/components/myShooping/MyShooping.jsx
@@ -8,7 +8,7 @@ import { useSelector } from 'react-redux';
 import { getPurchaseByUser } from '../../redux/actions/actions';
 
 
- const URL = 'http://localhost:3001'
+ //const URL = 'http://localhost:3001'
 //const URL = "https://quirkz.up.railway.app"
 
 const MyShopping = () => {
@@ -18,6 +18,16 @@ const MyShopping = () => {
   const purchaseItems = useSelector((state) => state.purchaseByUser)
   const navigate = useNavigate();
 
+  console.log(purchaseItems)
+
+  let myItems = []
+  for (let shop of purchaseItems) {
+    for (let item of shop.items){
+      myItems.push(item)
+    }
+  }
+
+  console.log(myItems)
   //const purchaseItems = [{id: 1, image: "https://images.asos-media.com/products/hugo-match-it-logo-sliders-in-black/204318769-1-black",
                         //   name: "HUGO Match It logo sliders in black", order: "25034", date: "12-11-2023", total: "$78.00"},
                         // {id: 2, image: "https://images.asos-media.com/products/nike-initiator-sneakers-in-white-and-gray/204917959-1-grey",
@@ -36,38 +46,39 @@ const MyShopping = () => {
     <div className={s["purchase-items"]}>
       <h2 className={s["purchase-items-header"]}>My Shopping: </h2>
       
-      {purchaseItems.length === 0 && (
+      {myItems.length === 0 && (
         <div className={s["purchase-items-empty"]}>Make a first buy </div>
       )}
 
       <div>
-        {purchaseItems.map((product) => (
+        {myItems.map((product) => (
           <div key={product.id} className={s["purchase-items-list"]}>
-            
-            <div className={s["purchase-items-name"]}>
-              <h2>  
-              {product.name}
-              </h2>
-              </div>
-              <div className={s["purchase-items-order"]}>
-              <h3>  
-              order: {product.order}
-              </h3>
-              </div>
+
             <div className={s["purchase-items-date"]}>
             <h3>  
               {product.date}
             </h3>
             </div>
+            <div className={s["purchase-items-name"]}>
+                <h3>
+                  {product.name}
+                </h3>          
+            </div>
+              <div className={s["purchase-items-order"]}>
+              <h3>  
+              {product.quantity}
+              </h3>
+              </div>
+            
              <div className={s["purchase-items-price"]}>
             <h3>
-              {product.total}
+              {product.price}
             </h3>
               </div>
               <div>
               <button 
               className={s["purchase-items-button"]} 
-              onClick={() => handleReview(product.id)}
+              onClick ={() =>handleReview(product.id)}
               >Qualify</button>
               </div>
           </div>
--- Client/src/components/purchase/Purchase.jsx
+++ Client/src/components/purchase/Purchase.jsx
@@ -2,33 +2,59 @@
 import s from './Purchase.module.css'
 //import transporter from '../../functions/sendMails';
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+
+
+
 
 
  const URL = "http://localhost:3001"
 //const URL = "https://quirkz.up.railway.app"
 
-const SuccessPayment = ({cartItems}) => {
+const SuccessPayment = () => {
+    const {search}= useLocation();
     const navigate = useNavigate();
     const userCart = useSelector((state) => state.userCart);
-   
-     const resPurchase = useSelector((state) => state.purchase)
-     console.log(resPurchase)
-   /*   const resPurchase = {collector_id: "24324", date_created: "2023-12-10"} */
-     const User = useSelector((state) => state.activeUser)
+    const User = useSelector((state) => state.activeUser)
+    console.log(userCart)
 
-     //console.log(User)
-    //const User = {name:"Luis", lastname: "Lenis", email: "llenis73@gmail.com"}
+    const getCollectionId = (search) => {
+      const params = search.split('&');
+      
+      for (let i = 0; i < params.length; i++) {
+        const param = params[i];
+        const [key, value] = param.split('=');
+        if (key === '?collection_id') {
+          // Return the value of the parameter
+          return value;
+        }
+      }
+      return null
+
+    }
+
+    const collector_Id = getCollectionId(search)
+    
+
+     const today = new Date();
+     const formattedDate = today.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    });
+    
+      
     
      const totalPrice = Math.floor(userCart.reduce((price, item) => price + item.quantity * item.price, 0))
     
+    
      let items = []
      for (let product of userCart){
-        items.push({name:product.name, quantity:product.quantity, price: product.price})
+        items.push({id: product.id, name:product.name, quantity:product.quantity, price: product.price, date:formattedDate})
      }
-     const purchase = {items: items, totalAmount: totalPrice, order:resPurchase.collector_id, 
-                    date: resPurchase.date_created, email:User.email, name: User.name, lastname: User.lastname} ;
+     const purchase = {items: items, totalAmount: totalPrice, order: collector_Id, 
+                    date: formattedDate, email:User.email, name: User.name, lastname: User.lastname} ;
      
     
   
@@ -44,11 +70,11 @@ const SuccessPayment = ({cartItems}) => {
         <h1 className={s["purchase-items-header"]}>Shopping summary: </h1>
 
         <h2>User</h2>
-        <h3>Name: {User.name}&nbsp;{User.lastname} </h3>
+        <h3>{User.name && `Name: ${User.name} ${User.lastname}`} </h3>
         <h3>Email: {User.email}</h3>
         
-        <h2>Order: {resPurchase.collector_id} </h2>
-        <h3>Date: {resPurchase.date_created} </h3>
+        <h2>Order: {collector_Id} </h2>
+        <h3>Date: {formattedDate} </h3>
         <h3>Total: ${totalPrice}</h3>
         <h3>Currency: COP</h3>
         <h3>Status: Paid</h3>
